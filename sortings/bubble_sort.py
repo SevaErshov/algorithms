e2e97d08be7907de5a12220951b40a77fad89102
SevaErsh <erschidvs@yandex.ru>
2023-01-22 17:22:04 +0300

    add bubble sort algorithm

---
+++ sortings/bubble_sort.py
@@ -0,0 +1,23 @@
+#O(n^2)
+
+
+def bubble_sort(array):
+    length = len(array)
+    for i in range(length):
+        for j in range(0, length - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+
+#Если j > j + 1 элемент, то мы меняем их местами. При каждой итерации цикла с i вверх поднимается один элемент. Таким образом, последние i элементов мы уже не трогаем. 
+#Улучшенная версия: проверяем один раз, не подан ли нам на вход уже отсортированный массив
+
+def bubble_sort(array):
+    length = len(array)
+    swapped = False
+    for i in range(length):
+        for j in range(0, length - i - 1):
+            if array[j] > array[j + 1]:
+                swapped = True
+                array[j], array[j + 1] = array[j + 1], array[j]
+        if swapped:
+            return
